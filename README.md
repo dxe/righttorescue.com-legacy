b0f966917a2f182c98aaa9fc4dd45c24064b8bf7
Jake Hobbs <jakehobbs@gmail.com>
2021-09-13 13:25:17 -0700

    use partials for header & footer, add prettier

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # righttorescue.com
 
 A static website hosted on S3 and served via CloudFront.
+
+Run `npm run fmt` to format source code.
+
+Run `gulp` to build into the `dist` directory.
+
+Commits to master are automatically deployed.
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,51 @@
-const gulp = require('gulp');
-const minify = require('gulp-minify');
-const csso = require('gulp-csso');
+const gulp = require("gulp");
+const minify = require("gulp-minify");
+const csso = require("gulp-csso");
+const fileinclude = require("gulp-file-include");
 
-gulp.task("html", function() {
+gulp.task("html", function () {
   return gulp
     .src(["*.html"])
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
     .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task("css", function() {
-  return gulp
-    .src(["css/**"])
-    .pipe(csso())
-    .pipe(gulp.dest("./dist/css"));
+gulp.task("css", function () {
+  return gulp.src(["css/**"]).pipe(csso()).pipe(gulp.dest("./dist/css"));
 });
 
-gulp.task("img", function() {
-  return gulp
-    .src(["img/**"])
-    .pipe(gulp.dest("./dist/img"));
+gulp.task("img", function () {
+  return gulp.src(["img/**"]).pipe(gulp.dest("./dist/img"));
 });
 
-gulp.task("js", function() {
+gulp.task("js", function () {
   return gulp
     .src(["js/**"])
-    .pipe(minify({
-      ext:{
-            min:'.js'
-        }, noSource: true}))
+    .pipe(
+      minify({
+        ext: {
+          min: ".js",
+        },
+        noSource: true,
+      })
+    )
     .pipe(gulp.dest("./dist/js"));
 });
 
-gulp.task("vendor", function() {
-  return gulp
-    .src(["vendor/**"])
-    .pipe(gulp.dest("./dist/vendor"));
+gulp.task("vendor", function () {
+  return gulp.src(["vendor/**"]).pipe(gulp.dest("./dist/vendor"));
 });
 
-gulp.task("docs", function() {
-  return gulp
-    .src(["docs/**"])
-    .pipe(gulp.dest("./dist/docs"));
+gulp.task("docs", function () {
+  return gulp.src(["docs/**"]).pipe(gulp.dest("./dist/docs"));
 });
 
-gulp.task('default', gulp.parallel('html', 'css', 'img', 'js', 'vendor', 'docs'));
+gulp.task(
+  "default",
+  gulp.parallel("html", "css", "img", "js", "vendor", "docs")
+);
